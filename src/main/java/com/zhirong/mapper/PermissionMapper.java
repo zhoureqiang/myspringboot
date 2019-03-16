f04af06c5aaea72ceccce3fb298944cad57b5a0e
zhoureqiang <zhou2493re@163.com>
2019-03-16 23:13:38 +0800

    配置mybatis插件自动生成mapper和xml

---
+++ src/main/java/com/zhirong/mapper/PermissionMapper.java
@@ -0,0 +1,17 @@
+package com.zhirong.mapper;
+
+import com.zhirong.entity.Permission;
+
+public interface PermissionMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Permission record);
+
+    int insertSelective(Permission record);
+
+    Permission selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Permission record);
+
+    int updateByPrimaryKey(Permission record);
+}
